f62c6b104ecfde26c48201f80ee89dade584b19e
rufus hamby <rufus.hamby@gmail.com>
2022-07-11 13:29:18 -0500

    updated operators

--- operatorexercise/Program.cs
+++ operatorexercise/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace operatorexercise
+{
+
+    class program
+    {
+      
+
+        static void Main(string[] arg)
+        {
+
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remander {mod}");
+            //-----------------exercise 2---------------------//
+
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfcircle = pi *(r*r);
+
+            Console.WriteLine($"The area of a circle with radius of {r} is {areaOfcircle}");
+
+        }
+    }
+
+}
+
 
